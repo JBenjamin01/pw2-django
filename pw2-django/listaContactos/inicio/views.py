2466f9f1779f64a496fe363a0817e840a6580619
JBenjamin01 <jmamanices@unsa.edu.pe>
2024-06-08 23:48:00 -0500

    Nueva vista anotherView asignada a la ruta con la cadena /otro/

--- pw2-django/listaContactos/inicio/views.py
+++ pw2-django/listaContactos/inicio/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwargs):
     return HttpResponse("<h1>Hola mundo desde Django</h1>")
+def anotherView(request):
+    return HttpResponse("<h1>Sólo otra página más</h1>")
