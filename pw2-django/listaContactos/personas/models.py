594256b238c7a6023222375456db1725c6a117cc
JBenjamin01 <jmamanices@unsa.edu.pe>
2024-06-08 23:42:00 -0500

    Solución de la validación con una función que verifique la entrada de edad

--- pw2-django/listaContactos/personas/models.py
+++ pw2-django/listaContactos/personas/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
+def validate_age(value):
+    if value < 0 or value > 120:
+        raise ValidationError('Edad debe estar entre 0 y 120.')
+    
 # Create your models here.
 class Persona(models.Model):
     nombres = models.TextField(max_length = 100)
     apellidos = models.TextField(max_length = 100)
-    edad = models.IntegerField(max_digits = 3)
+    edad = models.IntegerField(validators=[validate_age])
     donador = models.BooleanField()
